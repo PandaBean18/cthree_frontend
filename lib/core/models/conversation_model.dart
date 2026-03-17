6d8391aab8315b3514fd899d99cbe0280b299af5
raghav <raghapb34n@gmail.com>
2026-03-17 20:59:43 +0530

    conversation model

---
+++ lib/core/models/conversation_model.dart
@@ -0,0 +1,32 @@
+class Message {
+  String body;
+  DateTime createdAt;
+  DateTime? readAt;
+  String senderId;
+
+  Message({required this.body, required this.createdAt, this.readAt, required this.senderId});
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(body: json['body'], createdAt: DateTime.parse(json['created_at']), senderId: json['sender_id'], readAt: json['read_at'] != null ? DateTime.parse(json['read_at']) : null);
+  }
+}
+
+class ConversationModel {
+  String campaignId;
+  String creatorId;
+  String sponsorId;
+  String campaignTitle;
+  Message? latestMessage;
+
+  ConversationModel({ required this.campaignId, required this.creatorId, required this.sponsorId, required this.campaignTitle, this.latestMessage });
+
+  factory ConversationModel.fromJson(Map<String, dynamic> json) {
+    return ConversationModel(
+      campaignId: json['campaign']['id'],
+      creatorId: json['creator_id'],
+      sponsorId: json['sponsor_id'],
+      campaignTitle: json['campaign']['title'],
+      latestMessage: json['latest_message'] == null ? null : Message.fromJson(json['latest_message'])
+    );
+  }
+}
